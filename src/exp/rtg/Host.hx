fa723ddde6e5c37ded6abd29b7da6327aee497bd
Kevin Leung <kevinresol@gmail.com>
2020-05-05 15:43:50 +0800

    Attempt test flow in another way

--- src/exp/rtg/Host.hx
+++ src/exp/rtg/Host.hx
@@ -8,13 +8,12 @@ using tink.CoreApi;
 
 @:access(exp.rtg)
 class Host {
-	
 	public final rooms:Rooms;
-	
-	public static function create(createServer:()->Promise<Server>, roomTypeSupported):Promise<Host> {
+
+	public static function create(createServer:() -> Promise<Server>, roomTypeSupported):Promise<Host> {
 		return createServer().next(Host.new.bind(roomTypeSupported));
 	}
-	
+
 	function new(roomTypeSupported:String->Bool, server:Server) {
 		rooms = new Rooms();
 		server.connected.handle(client -> {
@@ -22,14 +21,14 @@ class Host {
 			guest.data.handle(function(command) {
 				switch command {
 					case Success(Metadata(CreateRoom(type))):
-						if(roomTypeSupported(type)) {
+						if (roomTypeSupported(type)) {
 							var room = new Room(type);
 							rooms.add(room);
 							guest.send(Metadata(RoomCreated(room.id, type)));
 						} else {
 							guest.send(Metadata(RoomCreateFailed(type, UnsupportedType)));
 						}
-						
+
 					case Success(Metadata(JoinRoom(id))):
 						switch rooms.get(id) {
 							case null:
@@ -40,7 +39,7 @@ class Host {
 								roomGuest.disconnected.handle(_ -> room.guests.remove(roomGuest.id));
 								guest.send(Metadata(RoomJoined(id, roomGuest.id, room.type)));
 						}
-						
+
 					case Success(Metadata(RejoinRoom(id, as))):
 						switch rooms.get(id) {
 							case null:
@@ -54,19 +53,19 @@ class Host {
 										guest.send(Metadata(RoomRejoined(id, as, room.type)));
 								}
 						}
-						
+
 					case Success(Metadata(LeaveRoom(id))):
 						switch rooms.get(id) {
 							case null:
 								guest.send(Metadata(RoomLeaveFailed(id, NotExist)));
 							case room:
-								if(room.guests.remove(id)) {
+								if (room.guests.remove(id)) {
 									guest.send(Metadata(RoomLeft(id)));
 								} else {
 									guest.send(Metadata(RoomLeaveFailed(id, NotJoined)));
 								}
 						}
-						
+
 					case Success(Metadata(CloseRoom(id))):
 						switch rooms.get(id) {
 							case null:
@@ -75,65 +74,63 @@ class Host {
 								room.close();
 								// broadcast performed by room
 						}
-						
+
 					case Success(Data(id, data)):
-						// handled in room
-						
+					// handled in room
+
 					case Failure(e):
 						// invalid uplink
 				}
 			});
 		});
 	}
-	
-	public function destroy() {
-		
-	}
+
+	public function destroy() {}
 }
 
 @:access(exp.rtg)
-private class Room {
+class Room {
 	static var ids = 0;
-	
+
 	public final id:Int;
 	public final type:String;
 	public final guests:RoomGuests;
-	
+
 	final pendingRejoin:Map<Int, PromiseTrigger<RoomGuest>>;
 	var guestIds = 0;
-	
+
 	public function new(type) {
 		this.id = ids++;
 		this.type = type;
 		this.guests = new RoomGuests();
 		this.pendingRejoin = new Map();
 	}
-	
+
 	public inline function broadcast(data:Chunk) {
 		_broadcast(Data(id, data));
 	}
-	
+
 	public function close() {
 		_broadcast(Metadata(RoomClosed(id)));
 		destroy();
 	}
-	
+
 	public function waitForRejoin(guest:RoomGuest, expiry:Future<Noise>):Promise<RoomGuest> {
 		var trigger = Promise.trigger();
 		pendingRejoin[guest.id] = trigger;
 		return trigger;
 	}
-	
-	
+
 	function _broadcast(message:Downlink) {
 		var serialized = tink.Json.stringify(message);
-		for(guest in guests) guest.guest.client.send(serialized); // bypass serializations
+		for (guest in guests)
+			guest.guest.client.send(serialized); // bypass serializations
 	}
-	
+
 	inline function nextGuestId() {
 		return guestIds++;
 	}
-	
+
 	inline function destroy() {
 		// TODO: cleanup
 	}
@@ -142,9 +139,9 @@ private class Room {
 @:access(tink.state)
 class Rooms {
 	public final created:Signal<Room>;
-	
+
 	final map:ObservableMap<Int, Room> = new ObservableMap([]);
-	
+
 	public function new() {
 		created = new Signal(cb -> {
 			map.changes.handle(change -> switch change {
@@ -153,15 +150,15 @@ class Rooms {
 			});
 		});
 	}
-	
+
 	inline function get(id:Int):Room {
 		return map.get(id);
 	}
-		
+
 	inline function add(room:Room) {
 		map.set(room.id, room);
 	}
-	
+
 	inline function remove(id:Int) {
 		map.remove(id);
 	}
@@ -171,34 +168,36 @@ class Rooms {
 class RoomGuests {
 	public final connected:Signal<RoomGuest>;
 	public final disconnected:Signal<RoomGuest>;
-	
+
 	final array:ObservableArray<RoomGuest> = new ObservableArray();
-	
+
 	public function new() {
 		connected = new Signal(cb -> {
 			array.changes.handle(change -> switch change {
-				case Insert(index, values): for(v in values) cb.invoke(v);
+				case Insert(index, values): for (v in values)
+						cb.invoke(v);
 				case _:
 			});
 		});
 		disconnected = new Signal(cb -> {
 			array.changes.handle(change -> switch change {
-				case Remove(index, values): for(v in values) cb.invoke(v);
+				case Remove(index, values): for (v in values)
+						cb.invoke(v);
 				case _:
 			});
 		});
 	}
-	
+
 	public inline function iterator()
 		return array.values();
-	
+
 	public inline function add(guest:RoomGuest) {
 		array.push(guest);
 	}
-		
+
 	public function remove(id:Int):Bool {
-		for(guest in array.values()) {
-			if(guest.id == id) {
+		for (guest in array.values()) {
+			if (guest.id == id) {
 				array.remove(guest);
 				return true;
 			}
@@ -210,19 +209,19 @@ class RoomGuests {
 class HostGuest {
 	public final disconnected:Future<Option<Error>>;
 	public final data:Signal<Outcome<Uplink, Error>>;
-	
+
 	final client:Client;
-	
+
 	public function new(client:Client) {
 		this.client = client;
 		disconnected = client.disconnected;
-		data = client.data.map(v -> tink.Json.parse((v:Uplink)));
+		data = client.data.map(v -> tink.Json.parse((v : Uplink)));
 	}
-	
+
 	public function send(message:Downlink):Promise<Noise> {
 		return client.send(tink.Json.stringify(message));
 	}
-	
+
 	public function disconnect():Future<Noise> {
 		return client.disconnect();
 	}
@@ -233,25 +232,25 @@ class RoomGuest {
 	public final id:Int;
 	public final disconnected:Future<Option<Error>>;
 	public final data:Signal<Chunk>;
-	
+
 	final room:Room;
 	final guest:HostGuest;
-	
+
 	public function new(guest:HostGuest, room:Room, ?id:Int) {
 		this.room = room;
 		this.guest = guest;
 		this.id = id == null ? room.nextGuestId() : id;
 		disconnected = guest.disconnected;
 		data = guest.data.select(v -> switch v {
-			case Success(Data(id, data)) if(id == room.id): Some(data);
+			case Success(Data(id, data)) if (id == room.id): Some(data);
 			case _: None;
 		});
 	}
-	
+
 	public function send(data:Chunk):Promise<Noise> {
 		return guest.send(Data(room.id, data));
 	}
-	
+
 	public function disconnect():Future<Noise> {
 		return guest.disconnect();
 	}
